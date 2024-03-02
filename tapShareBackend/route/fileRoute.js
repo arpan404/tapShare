4b08033a463d2a433c5ab40479e51a46ab7b0b27
arpan404 <itsmearpan1@gmail.com>
2024-03-02 14:54:27 +0545

    Removed old backend

--- tapShareBackend/route/fileRoute.js
+++
@@ -1,9 +0,0 @@
-const { sendFiles } = require("../controller/email/fileController");
-
-const router = require("express").Router();
-const { multer, storage } = require("../services/multerConfig");
-const upload = multer({ storage: storage });
-
-router.route("/sendFile").post(upload.array("files"), sendFiles);
-
-module.exports = router;
